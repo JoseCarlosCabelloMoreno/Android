3950f02696bb6ace3a85621da1d475156eb68889
JoseCarlosCabelloMoreno <jose.cabello.moreno98dev@gmail.com>
2022-02-09 12:22:26 -0600

    logica agregada

--- app/src/main/java/com/example/edad_canina/MainActivity.kt
+++ app/src/main/java/com/example/edad_canina/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.edad_canina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var boton=findViewById<Button>(R.id.boton)
+        var edad=findViewById<TextView>(R.id.edad)
+        var resultado=edad.text.toString()
+        var edadResultado=resultado.toInt()
+        boton.setOnClickListener{
+            if (resultado.isNotEmpty()){
+                edadResultado=edadResultado*7
+                edad.text=edadResultado.toString()
+            }else{
+                Toast.makeText(this, "Ingresa un numero", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
